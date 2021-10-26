c0fbda552c8901a39eb1a6261202155c5e11f9ac
LoganCoats22 <logancoats222@gmail.com>
2021-10-26 08:10:44 -0500

    Publishing to Github
    Assignment finished.

--- Coats_FormatName/ViewController.swift
+++ Coats_FormatName/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet var displayLabel: UILabel!
+    
+    
+    @IBAction func onClickSubmit(_ sender: UIButton) {
+        let name1: String? = firstNameTextField.text
+        let name2:String? = lastNameTextField.text
+        displayLabel.text =  name2! + ", " + name1!
+    }
+    
+    
+    @IBAction func onClickReset(_ sender: UIButton) {
+        firstNameTextField.text = ""
+        lastNameTextField.text = ""
+        displayLabel.text = ""
+        becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
